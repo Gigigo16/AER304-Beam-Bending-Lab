dec30369b50f58ef91a142b9651055236e712403
smurugan23 <sritejas.murugan@mail.utoronto.ca>
2024-03-23 03:15:43 -0400

    Ibeam stuff

--- src/main.py
+++ src/main.py
@@ -41,19 +41,51 @@ print("=========================================")
 print("Beginning Analysis...")
 print("=========================================")
 
-# parsed_data = StrainProcess(raw_data, test_num, sample_dimensions)
+# Analysis for theoretical I-beam deflections:
+I = 1457918.40
+E = 72000
+L = 400
+H = 76
+b = 8.86
 
-# modulus = ModulusProcess(parsed_data, test_num)
+nx, ny = (L+1, H*2+1)
+x = np.linspace(-L/2, L/2, nx)
+y = np.linspace(-H/2, H/2, ny)
+xv, yv = np.meshgrid(x, y)
 
-# yield_strength = YieldProcess(parsed_data, test_num, modulus)
-# ultimate_strength = UltimateProcess(parsed_data, test_num)
+# 3-pnt:
+max_F3 = 17238.5017
+M_max = max_F3*L/2
+slope = M_max/(L/2)
+M3 = M_max - slope*abs(x)
+sig3_xx = -1*M3*yv/I
+eps3_xx = sig3_xx/E
+Q = b/2*(H**2/4 - yv**2)
+sig3_xy = max_F3/2 * Q/I/b
+eps3_xy = sig3_xy/E
+eps3_xy[:,200:-1] = -1*eps3_xy[:,200:-1]
+plt.contourf(xv, yv, sig3_xx)
+plt.colorbar()
+plt.show()
 
-# print("Poisson's Ratio:")
-# print(modulus[1]/modulus[2])
 
-# print(parsed_data)
-# print("Analysis Complete...")
-# print("=========================================")
+# 4-pnt:
+max_F4 = 17806.9821
+M4 = max_F4*140 # 400/2 - 120/2
+sig4_xx = -M4*yv/I
+eps4_xx = sig4_xx/E
+Q = b/2*(H**2/4 - yv**2)
+sig4_xy = max_F4/2 * Q/I/b
+eps4_xy = sig4_xy/E
+eps4_xy[:,200:-1] = -1*eps4_xy[:,200:-1]
+plt.contourf(xv, yv, sig4_xx)
+plt.colorbar()
+plt.show()
+
+
+
+print("Analysis Complete...")
+print("=========================================")
 
 # GRAPHING DATA
 ########################
